4f7d5e8675bec81bfea6fd85f52d004b2be189d8
Corwin <corstreet@gmail.com>
2020-02-12 17:59:12 -0800

    proof of concept for the spotify search

--- liri.js
+++ liri.js
@@ -1,1 +1,20 @@
+//Requirements
 require("dotenv").config();
+var axios = require("axios");
+
+var Spotify = require("node-spotify-api");
+
+//bring in spotify creds and initialize
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+//store song as string
+var songToSearch = process.argv[2];
+// searchSpotify(songToSearch);
+//create an initial POC that can search spotify just by providing the song arg
+spotify.search({ type:'track', query: songToSearch }, function(err, res){
+  if (err) {
+    return console.log('The following error occurred while search spotify: ' + err);
+  }
+  console.log(res.tracks.items[0].album.artists[0].name);
+});
